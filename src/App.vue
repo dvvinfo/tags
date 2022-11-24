e282ccac878ee5721b95b798a27c3b7752a6d51d
dvvinfo <dvvinfo@mail.ru>
2022-11-24 17:23:51 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,79 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-main>
+      <text-tags
+        :tags="tags"
+        :alignment="(alignment = true)"
+        :alignmentLeftClass="alignmentLeftClass"
+        :alignmentBetweenClass="alignmentBetweenClass"
+      />
+      <text-tags
+        :tags="tags"
+        :alignment="(alignment = false)"
+        :alignmentLeftClass="alignmentLeftClass"
+        :alignmentBetweenClass="alignmentBetweenClass"
+      />
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<script>
+import TextTags from "@/components/TextTags.vue";
+export default {
+  name: "App",
+  components: {
+    TextTags,
+  },
+  data: () => ({
+    tags: [
+      {
+        text: "mdi-domain",
+        icons: "mdi-domain",
+      },
+      {
+        text: "text2",
+        icons: "mdi-arrow-up-bold-box-outline",
+      },
+      {
+        text: "text2",
+        icons: "mdi-message-text",
+      },
+      {
+        text: "text3",
+        icons: "mdi-open-in-new",
+      },
+      {
+        text: "text4",
+        icons: "mdi-email",
+      },
+    ],
+    alignment: false,
+    alignmentLeftClass: "left",
+    alignmentBetweenClass: "between",
+  }),
+};
+</script>
---
+++ src/components/TextTags.vue
@@ -0,0 +1,98 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col class="mb-5" cols="12">
+        <h2 class="headline font-weight-bold">Tags</h2>
+
+        <v-row justify="center mt-3">
+          <div
+            class="tags-wrapper"
+            :class="[alignment ? alignmentLeftClass : alignmentBetweenClass]"
+          >
+            <div class="tags-list">
+              <div class="tags-item" v-for="(tag, i) in tags" :key="i">
+                <v-icon v-if="tag.icons" class="subheading mx-1">
+                  {{ tag.icons }}
+                </v-icon>
+                <span class="mx-1">{{ tag.text }}</span>
+                <div>
+                  <v-icon class="subheading icon">mdi-circle-small</v-icon>
+                </div>
+              </div>
+            </div>
+          </div>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "TextTags",
+  props: {
+    tags: {
+      type: Array,
+    },
+    alignment: {
+      type: Boolean,
+    },
+    alignmentLeftClass: {
+      type: String,
+    },
+    alignmentBetweenClass: {
+      type: String,
+    },
+  },
+
+  data: () => ({}),
+};
+</script>
+<style lang="scss">
+.tags-wrapper {
+  display: flex;
+  width: 100%;
+}
+.tags-list {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  max-width: 800px;
+  overflow: hidden;
+  :last-child.tags-item .icon {
+    display: none;
+  }
+  :last-child.tags-item {
+    margin-right: 0;
+  }
+}
+.tags-item {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  margin-right: 10px;
+  width: 100%;
+}
+.icon {
+  position: absolute !important;
+  top: 0;
+  right: -11%;
+}
+.left {
+  justify-content: flex-start;
+}
+.between {
+  justify-content: space-between;
+  .tags-list {
+    max-width: none;
+  }
+}
+@media screen and (max-width: 678px) {
+  .tags-item {
+    max-width: 150px;
+    flex-shrink: 0;
+  }
+}
+</style>
